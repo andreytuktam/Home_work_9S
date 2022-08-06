37fc3479786813fa447cbc71ec8346a3c1da6e7e
Andrey Tuktam <tuktam@mail.ru>
2022-08-06 11:02:31 +0700

    Home_work_9S

--- 64/Program.cs
+++ 64/Program.cs
@@ -3,13 +3,13 @@
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 // M = 4; N = 8. -> ""4, 6, 7, 8""
 
-Console.Write("Введите M: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите N: ");
-int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
 string PrintNumbers(int start, int end)
 {
     if (start == end) return start.ToString();
     return (start + ", " + PrintNumbers(start+1,end));
 }
-Console.WriteLine(PrintNumbers(M,N));
+Console.WriteLine(PrintNumbers(m, n));
--- 66/Program.cs
+++ 66/Program.cs
@@ -3,15 +3,15 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите M: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите N: ");
-int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
 int PrintNumbers(int start, int end)
 {
     if (start == end) return start;
     if (start > end) return (start + PrintNumbers(start - 1, end)); 
     return (start + PrintNumbers(start + 1, end));
 }
-Console.WriteLine(PrintNumbers(M, N));
+Console.WriteLine(PrintNumbers(m, n));
 
